849d4fc781039c498d5aaad3d0b3414d1d8e63a1
erickarodoom <erickarodom@gmail.com>
2021-05-23 19:25:15 -0700

    initial commit 3

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -33238,7 +33238,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53233" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53422" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
